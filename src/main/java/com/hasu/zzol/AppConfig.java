04b5f4b01efd78d02fccaa3a3c2a9542cafd8d62
iioo3356 <iioo3356@gmail.com>
2024-04-05 20:58:12 +0900

    feat: Container, Bean 적용

--- src/main/java/com/hasu/zzol/AppConfig.java
+++ src/main/java/com/hasu/zzol/AppConfig.java
@@ -1,11 +1,21 @@
 package com.hasu.zzol;
 
+import com.hasu.zzol.member.MemberRepository;
 import com.hasu.zzol.member.MemberService;
 import com.hasu.zzol.member.MemberServiceImpl;
 import com.hasu.zzol.member.MemoryMemberRepositoryImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepositoryImpl();
+    }
+
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepositoryImpl());
+        return new MemberServiceImpl(memberRepository());
     }
 }
--- src/test/java/com/hasu/zzol/member/MemberServiceTest.java
+++ src/test/java/com/hasu/zzol/member/MemberServiceTest.java
@@ -2,13 +2,15 @@ package com.hasu.zzol.member;
 
 import com.hasu.zzol.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberServiceTest {
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
-
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService = ac.getBean("memberService", MemberService.class);
     @Test
+    @DisplayName("회원가입 테스트")
     void sighUp() {
         Member member = new Member(1L, "1번 유저", 100L, "2024-16-38");
         memberService.signUp(member);
