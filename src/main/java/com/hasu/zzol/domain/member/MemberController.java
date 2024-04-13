e0536d90f46a176348a467a2e27217ba888c764b
iioo3356 <iioo3356@gmail.com>
2024-04-14 03:04:50 +0900

    chore: 회원 컨트롤러 공통 매핑 처리

--- src/main/java/com/hasu/zzol/domain/member/MemberController.java
+++ src/main/java/com/hasu/zzol/domain/member/MemberController.java
@@ -6,33 +6,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@RequestMapping("/member")
 @RestController
 @AllArgsConstructor
 public class MemberController {
     private final MemberService memberService;
 
-    @PostMapping("/member")
+    @PostMapping()
     public void signUp(@RequestBody MemberDto memberDto) {
         System.out.println(memberDto);
         memberService.signUp(memberDto);
     }
 
-    @GetMapping("/member/list")
+    @GetMapping("/list")
     public List<Member> getAll() {
         return memberService.findAll();
     }
 
-    @GetMapping("/member/{memberId}")
+    @GetMapping("/{memberId}")
     public Optional<Member> getMember(@PathVariable Long memberId) {
         return memberService.findMember(memberId);
     }
 
-    @PutMapping("/member/{memberId}")
+    @PutMapping("/{memberId}")
     public void putMember(@PathVariable Long memberId, @RequestBody MemberDto memberDto) {
         memberService.putMember(memberId, memberDto);
     }
 
-    @DeleteMapping("/member/{memberId}")
+    @DeleteMapping("/{memberId}")
     public void deleteMember(@PathVariable Long memberId){
         memberService.deleteMember(memberId);
     }
